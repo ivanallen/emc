9ec89c1273ae973c4a18ebaabcb991b94e837859
allen <1007729991@qq.com>
2018-09-12 19:36:56 +0800

    add template

--- item01/demo03.cpp
+++ item01/demo03.cpp
@@ -14,17 +14,17 @@
 using namespace emc::base;
 
 template<typename T>
-std::string f(T& param) {
+std::string f(T* param) {
 	return type_name<T>();
 }
 
 
 int main() {
 	int x = 27;
-	const int cx = x;
-	const int& rx = x;
+	int* cx = &x;
+	const int* rx = &x;
 
-	std::string tx = f(x);
+	std::string tx = f(&x);
 	std::string tcx = f(cx);
 	std::string trx = f(rx);
 
@@ -33,16 +33,16 @@ int main() {
 		{"variable", color::yellow},
 		{"comment", color::cyan},
 		{"default", cntl::def},
-		{"param_type", "T&"},
+		{"param_type", "T*"},
 		{"type_x", "int"},
-		{"type_cx", "const int"},
-		{"type_rx", "const int&"},
+		{"type_cx", "int*"},
+		{"type_rx", "const int*"},
 		{"tx", tx},
 		{"tcx", tcx},
 		{"trx", trx},
-		{"px", tx + "&"},
-		{"pcx", tcx + "&"},
-		{"prx", trx + "&"}
+		{"px", tx + "*"},
+		{"pcx", tcx + "*"},
+		{"prx", trx + "*"}
 	});
 
 	return 0;
